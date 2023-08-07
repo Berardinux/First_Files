a690fe7383cf4a2edb52e7ba411ff5ef563deb09
berardinux <berardinux@gmail.com>
2023-08-06 22:12:01 -0400

    22:11, 8-6-23

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+x = 10
+
+print ( type(x) )
+
+x = 1.7
+
+print ( type(x) )
+
+y = 11*x
+
+print (11*x)
+
+print ("Hello World")
+
+message = "Hello World"
+
+print (type(message))
+
+myBool = True
+
+print (type(myBool))
